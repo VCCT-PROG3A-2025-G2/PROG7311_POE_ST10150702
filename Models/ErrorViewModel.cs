317542196aae564e6c89243e8b59e93cba8fe5c4
JordanMuller039 <st10150702@vcconnect.edu.za>
2025-05-07 00:21:04 +0200

    Add project files.

---
+++ Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace PROG7311_POE_ST10150702.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
